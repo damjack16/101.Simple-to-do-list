9e9b329cc19798bbbac414fffcd52c987338403f
Damian Jackiewicz <damian.jackiewicz92@gmail.com>
2019-11-12 06:17:52 +0100

    change addTask function and removeTask function. Add renderTask function

--- main.js
+++ main.js
@@ -4,43 +4,58 @@ const searchInput = document.querySelector("input.search");
 const addBtn = document.querySelector("button");
 const span = document.querySelector("span");
 const ul = document.querySelector("ul");
+const toDolist = [];
 
-
+//ADD TASK FUNCTION
 const addTask = (e) => {
     e.preventDefault();
     const inputValue = addInput.value;
     if (inputValue === "") return //if input is empty - stop the function
     addInput.value = "";
     const task = document.createElement(`li`);
-    task.innerHTML = inputValue + ` <button class="delete">Usuń</button>`;
+    task.innerHTML = inputValue + ` <button>Usuń</button>`;
     ul.appendChild(task);
-    span.textContent++;
+    toDolist.push(task); //add new task to Array
+    ul.textContent = ""; //clear ul from elements, because we will add elements from Array toDoList
+    span.textContent = toDolist.length;
+    //forEach add id attribute to new elements of toDoList
+    renderTask();
 
-    const removeBtn = [...document.querySelectorAll(".delete")];
-    removeBtn.forEach((button) => {
-        button.addEventListener('click', removeTask)
-    })
+    task.querySelector("button").addEventListener('click', removeTask);
 
 }
 
+//REMOVE TASK FUNCTION
 const removeTask = (e) => {
     e.target.parentNode.remove();
-    span.textContent--;
+    const elementId = e.target.parentNode.id;
+    toDolist.splice(elementId, 1);
+    span.textContent = toDolist.length;
+    renderTask();
 }
 
-const searchTask = (e) => {
-    const searchValue = e.target.value.toLowerCase();
-    const liItems = document.querySelectorAll('li');
-    let tasks = [...liItems];
-    tasks = tasks.filter(task => task.textContent.toLowerCase().includes(searchValue)); //show li with text inside our search input
-    ul.textContent = "";
-    tasks.forEach((task) => {
-        ul.appendChild(task);
+//RENDERING TASKS IN ARRAY
+const renderTask = () => {
+    toDolist.forEach((element, index) => {
+        element.id = index;
+        ul.appendChild(element);
     })
-    span.textContent = tasks.length;
+
 }
 
+// const searchTask = (e) => {
+//     const searchValue = e.target.value.toLowerCase();
+//     const liItems = document.querySelectorAll('li');
+//     let tasks = [...liItems];
+//     tasks = tasks.filter(task => task.textContent.toLowerCase().includes(searchValue)); //show li with text inside our search input
+//     ul.textContent = "";
+//     tasks.forEach((task) => {
+//         ul.appendChild(task);
+//     })
+//     span.textContent = tasks.length;
+// }
 
-searchInput.addEventListener("input", searchTask);
+
+// searchInput.addEventListener("input", searchTask);
 
 form.addEventListener("submit", addTask);
